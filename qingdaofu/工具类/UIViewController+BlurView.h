0b02cd877e909699e7584d00c1705ce1300fe0c7
shiyongli <1525846137@qq.com>
2016-06-11 22:08:42 +0800

    tableView 弹出框categary封装完成

---
+++ qingdaofu/工具类/UIViewController+BlurView.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+BlurView.h
+//  qingdaofu
+//
+//  Created by zhixiang on 16/6/11.
+//  Copyright © 2016年 zhixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (BlurView)
+
+@end
---
+++ qingdaofu/工具类/UIViewController+BlurView.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+BlurView.m
+//  qingdaofu
+//
+//  Created by zhixiang on 16/6/11.
+//  Copyright © 2016年 zhixiang. All rights reserved.
+//
+
+#import "UIViewController+BlurView.h"
+
+@implementation UIViewController (BlurView)
+
+@end
