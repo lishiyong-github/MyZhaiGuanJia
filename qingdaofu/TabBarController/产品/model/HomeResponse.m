b08bb20b43e37ba5265d776ca019d8832e389507
shiyongli <1525846137@qq.com>
2016-06-21 20:09:58 +0800

    发布的刷新列表，不能停止刷新

---
+++ qingdaofu/TabBarController/产品/model/HomeResponse.m
@@ -0,0 +1,13 @@
+//
+//  HomeResponse.m
+//  qingdaofu
+//
+//  Created by zhixiang on 16/6/21.
+//  Copyright © 2016年 zhixiang. All rights reserved.
+//
+
+#import "HomeResponse.h"
+
+@implementation HomeResponse
+
+@end
