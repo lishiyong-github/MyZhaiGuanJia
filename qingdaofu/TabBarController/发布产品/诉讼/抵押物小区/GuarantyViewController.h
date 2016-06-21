90afa6bbdc0022b613b8b19af5d4a23432e79de0
shiyongli <1525846137@qq.com>
2016-06-21 13:06:51 +0800

    删除地图；发布融资的车牌选择－－成功；开始发布融资的抵押物地址选择

---
+++ qingdaofu/TabBarController/发布产品/诉讼/抵押物小区/GuarantyViewController.h
@@ -0,0 +1,13 @@
+//
+//  GuarantyViewController.h
+//  qingdaofu
+//
+//  Created by zhixiang on 16/6/21.
+//  Copyright © 2016年 zhixiang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface GuarantyViewController : BaseViewController
+
+@end
