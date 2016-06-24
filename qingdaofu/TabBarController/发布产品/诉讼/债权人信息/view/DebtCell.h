45a936a291e21fdab997c82f1808fae5ca5a3d5e
shiyongli <1525846137@qq.com>
2016-06-24 15:03:42 +0800

    发布诉讼－－债权人文件

--- qingdaofu/TabBarController/发布产品/诉讼/债权人信息/view/DebtCell.h
+++ qingdaofu/TabBarController/发布产品/诉讼/债权人信息/view/DebtCell.h
@@ -2,23 +2,12 @@
 //  DebtCell.h
 //  qingdaofu
 //
-//  Created by zhixiang on 16/5/18.
+//  Created by zhixiang on 16/6/24.
 //  Copyright © 2016年 zhixiang. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface DebtCell : UITableViewCell
-
-@property (nonatomic,strong) UILabel *debtNameLabel;
-@property (nonatomic,strong) UIButton *debtEditButton;
-@property (nonatomic,strong) UILabel *debtTelLabel;
-@property (nonatomic,strong) UILabel *debtAddressLabel;
-@property (nonatomic,strong) UILabel *debtAddressLabel1;
-@property (nonatomic,strong) UILabel *debtIDLabel;
-@property (nonatomic,strong) UIImageView *debtImageView1;
-@property (nonatomic,strong) UIImageView *debtImageView2;
-
-@property (nonatomic,assign) BOOL didSetupConstraints;
 
 @end
