8ac7dfe327c96c858eedf1f01d717c204088e543
shiyong_li <1525846137@qq.com>
2016-06-27 23:21:35 +0800

    发布成功－－跳转用户页面成功

---
+++ qingdaofu/工具类/UIViewController+SelectedIndex.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+SelectedIndex.m
+//  qingdaofu
+//
+//  Created by shiyong_li on 16/6/27.
+//  Copyright © 2016年 zhixiang. All rights reserved.
+//
+
+#import "UIViewController+SelectedIndex.h"
+
+@implementation UIViewController (SelectedIndex)
+
+@end
