38caac98643a52e1e3cc4dbfb59472ab040bba5f
shiyongli <1525846137@qq.com>
2016-06-27 19:56:28 +0800

    发布催收－债权人信息（传model）

---
+++ qingdaofu/TabBarController/产品/产品详情/产品信息/ProductsCheckDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductsCheckDetailViewController.h
+//  qingdaofu
+//
+//  Created by zhixiang on 16/6/27.
+//  Copyright © 2016年 zhixiang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ProductsCheckDetailViewController : BaseViewController
+
+@end
